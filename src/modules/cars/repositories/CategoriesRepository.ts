ae11f5b895d4e6e50dc3aaf3136c9962f1893bbf
Wilson Evangelista <wilson.evangelista22@gmail.com>
2023-09-09 19:18:47 -0300

    feature:create usecase cars

---
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -0,0 +1,47 @@
+import { Category } from "../model/Category"
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
+
+
+
+class CategoriesRepository implements ICategoriesRepository {
+
+  private categories: Category[];
+
+  private static INSTANCE: CategoriesRepository
+
+  constructor() {
+    this.categories = [];
+  }
+
+  public static getInstace(): CategoriesRepository{
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository()
+    }
+    return CategoriesRepository.INSTANCE
+  }
+
+  create({description,name}:ICreateCategoryDTO):void {
+  const category = new Category()
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date()
+  })
+
+    this.categories.push(category)
+
+  }
+
+  list(): Category[]{
+    return this.categories
+  }
+
+  findByName(name: string): Category | undefined {
+    const category = this.categories.find((category) => category.name === name)
+    return category
+  }
+
+}
+
+export {CategoriesRepository}
