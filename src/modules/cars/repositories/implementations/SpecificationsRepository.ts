c6565027e34176c4f43c18a87f9f633d7010940f
Wilson Evangelista <wilson.evangelista22@gmail.com>
2023-09-09 21:16:19 -0300

    feature: create useCase specifications

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -4,9 +4,19 @@ import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecific
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
-  constructor() {
-    this.specifications = []
+  private static INSTANCE: SpecificationsRepository
+
+ private constructor() {
+    this.specifications = [];
   }
+
+  public static getInstace(): SpecificationsRepository{
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository()
+    }
+    return SpecificationsRepository.INSTANCE
+  }
+
   create({ description, name }: ICreateSpecificationDTO): void {
     const specification = new Specification()
 
