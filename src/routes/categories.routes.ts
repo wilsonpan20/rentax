ecec253e886ae3bf1205766a33c45d32250c791a
Wilson Evangelista <wilson.evangelista22@gmail.com>
2023-09-09 22:36:45 -0300

    feature: create useCase upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 
 
@@ -23,9 +24,7 @@ categoriesRoutes.get('/', (request, response) => {
 })
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  const { file } = request
-  console.log(file)
-  return response.send()
+ return importCategoryController.handle(request,response)
 
 })
 
